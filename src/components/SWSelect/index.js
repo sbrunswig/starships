5938cd24e4c57add28345488e95a22dd1f21d69a
Scott Brunswig <sbrunswig@gmail.com>
2021-10-04 07:21:38 -0500

    comments

--- src/components/SWSelect/index.js
+++ src/components/SWSelect/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./select.css";
 
 const SwSelect = (props) => {
+  /* user selects manufacturer, call function passed as prop */
   const handleStarshipChange = (e) => {
     props.filterStarship(e.target.value);
   };
+
   return (
-    <div className="d-f pos-r ov-h select">
+    <div className="d-f ov-h pos-r select">
       <select className="bgc-gray900 c-white p- w-1/1" onChange={handleStarshipChange} tabIndex="0">
         <option value="All"> -- Select a manufacturer -- </option>
         {props.manufacturers &&
